7cafb8cc09273e4a9c73dd5b8bf987ebdb1a1f0c
Ansh <anshpradhancareer03@gmail.com>
2023-02-11 13:33:13 +0530

    made changes to next.config file

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
 module.exports = nextConfig;
 
 module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: [
       "cloudflare-ipfs.com",
